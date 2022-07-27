1c45b2d7fcff13ee4ec3dd83a67cbd04709b8094
Nicholas <nicholasjoven@gmail.com>
2022-07-27 17:13:00 -0400

    synced

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
 import express from 'express'
 
 const FRIENDS = [
-    {
-        id: 1,
-        name: 'Sam Samson',
-        email: 'sam@example.com'
-    },
-    {
-        id: 2,
-        name: 'Julie Juleson',
-        email: 'julie@example.com'
-    },
-    {
-        id: 3,
-        name: 'Son Sonson',
-        email: 'son@example.com'
-    },
+    { id: 1, name: 'Sam Samson', email: 'sam@example.com' },
+    { id: 2, name: 'Julia Juleson', email: 'julia@example.com' },
+    { id: 3, name: 'Son Sonson', email: 'son@example.com' }
 ]
 
 const PENDING_REQUESTS = [
-    {
-        from: 1,
-        senderName: 'Sam Samson'
-    },
-    {
-        from: 3,
-        senderName: 'Sam Samson'
-    },
+    { from: 1, senderName: 'Sam Samson' },
+    { from: 3, senderName: 'Son Sonson' }
 ]
 
 const app = express()
 
+// Think of app.get as a way of attaching addEventListener to your browser's address bar
 app.get('/', (req, res) => {
-    res.json({message: 'You successfully created your own server'})
+    res.json({ message: 'You successfully created your own server' })
 })
 
+// I want to type localhost:3005/friends and get a list of the friends in the array above
 app.get('/friends', (req, res) => {
     res.json(FRIENDS)
 })
 
-app.get('/blocked', (req, res) => {
-    res.json({})
-})
-
+// I want to type localhost:3005/friend_requests and see the pending requests
 app.get('/friend_requests', (req, res) => {
     res.json(PENDING_REQUESTS)
 })
 
 app.listen(3005, () => {
-    console.log('Friendzone server is running on PORT 3005')
+    console.log('Friendzone server is running on PORT 3001')
 })
